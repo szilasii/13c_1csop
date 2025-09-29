bcb1a6ac87c2864051480beef33f96471241780c
Szilasi István <szilasii78@gmail.com>
2025-09-29 15:09:16 +0200

    vege

--- src/controller.ts
+++ src/controller.ts
@@ -15,7 +15,7 @@ export const getDataFromId = (req: any, res: any) => {
         res.status(400).send("Hibás paraméter!")
         return
     }
-    const dog: IDog  = data.find((i: any) => i.id === id)
+    const dog: IDog = data.find((i: any) => i.id === id)
     if (!dog) {
         res.status(404).send("Nem található a törlendő index!")
         return
@@ -25,22 +25,22 @@ export const getDataFromId = (req: any, res: any) => {
 
 
 export const insertData = (req: Request, res: any) => {
-   
+
     if (!req.body) {
         res.status(400).send("Nem adott meg adatokat!")
         return
     }
-    
-    
+
+
     let dog: Dog = new Dog(req.body as unknown as IDog)
-    
+
     if (dog.nev === "" || dog.fajta === "") {
-         res.status(400).send("Nem adott meg adatokat!")
-     return
+        res.status(400).send("Nem adott meg adatokat!")
+        return
     }
 
-    
-    dog.id = Math.max(...data.map((d:any) => d.id)) + 1
+
+    dog.id = Math.max(...data.map((d: any) => d.id)) + 1
     data.push(dog)
     res.status(201).send(dog)
 }
@@ -60,7 +60,63 @@ export const deleteDataFromId = (req: any, res: any) => {
     res.status(204).send()
 }
 
+export const putData = (req: any, res: any) => {
+    let id = parseInt(req.params.id)
+    if (isNaN(id)) {
+        res.status(400).send("Hibás paraméter!")
+        return
+    }
+    const index = data.findIndex((i: any) => i.id === id)
+    if (index === -1) {
+        insertData(req, res)
+        return
+    }
 
+    if (!req.body) {
+        res.status(400).send("Nem adott meg adatokat!")
+        return
+    }
+
+
+    let reqDog: Dog = new Dog(req.body as unknown as IDog)
+
+    if (reqDog.nev === "" || reqDog.fajta === "") {
+        res.status(400).send("Nem adott meg adatokat!")
+        return
+    }
+    reqDog.id = id
+    data[index] = reqDog
+    res.status(201).send(data)
+}
+
+
+
+export const patchData = (req: any, res: any) => {
+    let id = parseInt(req.params.id)
+    if (isNaN(id)) {
+        res.status(400).send("Hibás paraméter!")
+        return
+    }
+    const index = data.findIndex((i: any) => i.id === id)
+    if (index === -1) {
+        res.status(404).send("A megadott id nem létezik")
+        return
+    }
+
+    if (!req.body) {
+        res.status(400).send("Nem adott meg adatokat!")
+        return
+    }
+
+    let reqDog: Dog = new Dog(req.body as unknown as IDog)
+
+    data[index].nev  = reqDog.nev || data[index].nev
+    data[index].fajta  = reqDog.fajta || data[index].fajta
+    data[index].eletkor  = reqDog.eletkor || data[index].eletkor
+    data[index].nem  = reqDog.nem || data[index].nem
+    data[index].kepUrl  = reqDog.kepUrl || data[index].kepUrl
+    res.status(201).send(data)
+}
 
 
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 
 
 import { Router } from "express"
-import { deleteDataFromId, getALLData, getDataFromId, insertData, run,  } from "./controller"
+import { deleteDataFromId, getALLData, getDataFromId, insertData, patchData, putData, run,  } from "./controller"
 
 
 const router: Router = Router()
@@ -11,4 +11,8 @@ router.get("/dogs", getALLData)
 router.get("/dogs/:id", getDataFromId)
 router.post("/dogs",insertData)
 router.delete("/dogs/:id",deleteDataFromId)
+router.put("/dogs/:id",putData)
+router.patch("/dogs/:id",patchData)
+
+
 export default router
