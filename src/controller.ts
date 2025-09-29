f1a72e44499b85e0dff8e2954e8a992983613c2b
Szilasi István <szilasii78@gmail.com>
2025-09-29 13:43:07 +0200

    add 2 functions

--- src/controller.ts
+++ src/controller.ts
@@ -1,4 +1,5 @@
 import data from "./data"
+import Dog, { IDog } from "./dog"
 
 export const run = (_req: any, res: any) => {
     res.status(200).send("A szerver fut")
@@ -7,18 +8,41 @@ export const run = (_req: any, res: any) => {
 export const getALLData = (_req: any, res: any) => {
     res.status(200).send(data)
 }
+
+export const getDataFromId = (req: any, res: any) => {
+    let id = parseInt(req.params.id)
+    if (isNaN(id)) {
+        res.status(400).send("Hibás paraméter!")
+        return
+    }
+    const dog: IDog  = data.find((i: any) => i.id === id)
+    if (!dog) {
+        res.status(404).send("Nem található a törlendő index!")
+        return
+    }
+    res.status(201).send(dog)
+}
+
+
 export const insertData = (req: Request, res: any) => {
-    console.log(req.body)
+   
     if (!req.body) {
         res.status(400).send("Nem adott meg adatokat!")
         return
     }
-    let dog: any = req.body
-    const index = data.length
-    dog.id = index + 1
+    
+    
+    let dog: Dog = new Dog(req.body as unknown as IDog)
+    
+    if (dog.nev === "" || dog.fajta === "") {
+         res.status(400).send("Nem adott meg adatokat!")
+     return
+    }
+
+    
+    dog.id = Math.max(...data.map((d:any) => d.id)) + 1
     data.push(dog)
-    console.log(data)
-    res.status(201).send(data)
+    res.status(201).send(dog)
 }
 export const deleteDataFromId = (req: any, res: any) => {
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,14 @@
 
 
 import { Router } from "express"
-import { deleteDataFromId, getALLData, insertData, run,  } from "./controller"
+import { deleteDataFromId, getALLData, getDataFromId, insertData, run,  } from "./controller"
 
 
 const router: Router = Router()
 
 router.get("/", run ) 
 router.get("/dogs", getALLData)
+router.get("/dogs/:id", getDataFromId)
 router.post("/dogs",insertData)
 router.delete("/dogs/:id",deleteDataFromId)
 export default router
